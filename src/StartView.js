7faa95e1eade08ce06cdd3a00a225247227a6ab1
Jonas Kay <jonas-kay@outlook.com>
2018-10-06 23:38:11 +0300

    Modify StartView.js
    Add initial styling

--- src/StartView.js
+++ src/StartView.js
@@ -3,10 +3,11 @@ import DirectionsMap from './DirectionsMap';
 
 export default class StartView extends Component {
   render() {
+    const buttonStyle = {height: '3rem'};
     return(
       <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
         <DirectionsMap tasks={this.props.tasks} />
-        <button onClick={() => this.props.onButtonClick()}>Start</button>
+        <button onClick={() => this.props.onButtonClick()} style={buttonStyle}>Start</button>
       </div>
     )
   }
