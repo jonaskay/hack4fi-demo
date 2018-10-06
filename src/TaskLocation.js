3cd4d45e69502209240b54d1ed423662b9442a64
Jonas Kay <jonas-kay@outlook.com>
2018-10-06 23:37:30 +0300

    Modify TaskLocation.js
    Initial styling

--- src/TaskLocation.js
+++ src/TaskLocation.js
@@ -11,12 +11,17 @@ export default class TaskLocation extends Component {
   }
 
   render() {
+    const buttonStyle = {height: '3rem'};
+
     return (
       <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+        <div style={{height: '3rem', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>Find this location</div>
         <div style={{width: '100%', flexGrow: 1, position: 'relative'}}>
           <Map onMapChange={map => this.initMarker(map)} lat={this.props.task.lat} lng={this.props.task.lng}  />
         </div>
-        <button onClick={() => this.props.onButtonClick()}>Arrived</button>
+        <button onClick={() => this.props.onButtonClick()} style={buttonStyle}>
+          I'm here! Let's start
+        </button>
       </div>
     );
   }
